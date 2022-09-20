2166fc23a6fe01388887d27b4b7c39fa9b6d75f9
Giossaurus <gihhskt@gmail.com>
2022-09-19 23:51:01 -0300

    Update app.js

--- typescript-curso-1-arquivos-iniciais/dist/js/app.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -1,1 +1,7 @@
 import { Negociacao } from "../../app/models/negociacao";
+
+const negociacao = new Negociacao(new Date(), 10 , 100);
+
+console.log(negociacao);
+negociacao.quantidade = 1000;
+console.log(negociacao);
